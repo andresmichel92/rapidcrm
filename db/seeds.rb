b9df7a0605a5bbc75f9840d8009ed41d0c308549
Andres Michel <andresmichel92@hotmail.com>
2015-02-26 23:25:56 +0000

    Stuff i don't remember

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do|i|
+  Lead.create!(name:"Daenerys Targaryen #{i}", company:"Khal Drogo's Company", location: "Khalessi's Land", phone: "5555555555") end
+
+50.times do|i|
+  Lead.create!(name:"Tywin Lanister #{i}", company:"King's Guard", location: "King's Landing", phone: "5555555555") end
+
+50.times do|i|
+  Lead.create!(name:"John Snow #{i}", company:"NightWatch", location: "NorthPole", phone: "5555555555") end
