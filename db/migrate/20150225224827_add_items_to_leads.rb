3089603c3537ca5bc612f55fdda7ac42f9d61e73
Andres Michel <andresmichel92@hotmail.com>
2015-03-02 17:52:01 +0000

    remove badone

--- db/migrate/20150225224827_add_items_to_leads.rb
+++ db/migrate/20150225224827_add_items_to_leads.rb
@@ -2,7 +2,7 @@ class AddItemsToLeads < ActiveRecord::Migration
   def change
     add_column :leads, :location, :string
     add_column :leads, :phone, :integer
-    add_column :leads, :badone, :string 
+    
     
   end
 end
--- db/migrate/20150225225415_remove_badone_from_leads.rb
+++
@@ -1,5 +0,0 @@
- RemoveBadoneFromLeads < ActiveRecord::Migration
-  def change
-    remove_column :leads, :badone
-  end
-end
