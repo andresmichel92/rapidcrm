1bbc7ef455d3e84a7fff961bcbf163138e1685b1
Andres Michel <andresmichel92@hotmail.com>
2015-03-06 23:45:46 +0000

    search date code added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225225415) do
+ActiveRecord::Schema.define(version: 20150306182856) do
 
   create_table "leads", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150225225415) do
     t.string   "location"
     t.integer  "phone"
     t.string   "badone"
+    t.date     "date"
   end
 
 end
